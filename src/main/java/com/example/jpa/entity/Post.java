3fb0f6b5ad0bcb72257c7b82795f7a7ac183289a
woochanhui <woochanhui@naver.com>
2026-06-05 11:23:18 +0900

    Post 내용 수정 및 업데이트

--- src/main/java/com/example/jpa/entity/Post.java
+++ src/main/java/com/example/jpa/entity/Post.java
@@ -1,7 +1,8 @@
 package com.example.jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -9,14 +10,36 @@ import java.time.LocalDateTime;
 public class Post {
 
     @Id
-    private long id; //
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private long id; //고유 번호
 
-    private String title;
+    @Column(nullable = false, length = 100)
+    private String title; //제목
 
-    private String content;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content; //내용
 
-    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private String author; //작성자
 
-    private LocalDateTime updataAt;
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt; //생성일자
 
+    @LastModifiedDate
+    private LocalDateTime updatedAt; //수정일자
+
+    public static Post create(String title, String content, String author) {
+        Post post = new Post();
+        post.title = title;
+        post.content = content;
+        post.author = author;
+        return post;
+    }
+
+    // 수정 메서드
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
